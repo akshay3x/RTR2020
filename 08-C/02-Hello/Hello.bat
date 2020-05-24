a99227f6af4824775629ff99c9fb06eacdb86776
akshay3x <chandawalear@gmail.com>
2020-05-25 01:05:43 +0530

    Hello World!

---
+++ 08-C/02-Hello/Hello.bat
@@ -0,0 +1,7 @@
+del Hello.obj
+del Hello.exe
+cl.exe /c /EHsc Hello.c
+
+link.exe Hello.obj
+
+Hello.exe
---
+++ 08-C/02-Hello/Hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("Akshay: Hello World!\n");
+	return (0);
+}
+
