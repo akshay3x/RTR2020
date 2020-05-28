9e0629605fe3ec2b8d8c371b4462f3700e4bfe51
akshay3x <chandawalear@gmail.com>
2020-05-28 23:33:13 +0530

    ++ --

---
+++ 08-C/07-Operators/01-ArithmeticOperators/03-IncrementDecrementOperators/IncrementDecrementOperators.bat
@@ -0,0 +1,7 @@
+del IncrementDecrementOperators.obj
+del IncrementDecrementOperators.exe
+cl.exe /c /EHsc IncrementDecrementOperators.c
+
+link.exe IncrementDecrementOperators.obj
+
+IncrementDecrementOperators.exe
