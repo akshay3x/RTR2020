bc709bb8878abadabd2d26cce6206bf189ec74bb
akshay3x <chandawalear@gmail.com>
2020-05-28 23:31:14 +0530

    Shorthand operators

---
+++ 08-C/07-Operators/01-ArithmeticOperators/02-CompoundAssignmentOperators/CompoundAssignmentOperators.bat
@@ -0,0 +1,7 @@
+del CompoundAssignmentOperators.obj
+del CompoundAssignmentOperators.exe
+cl.exe /c /EHsc CompoundAssignmentOperators.c
+
+link.exe CompoundAssignmentOperators.obj
+
+CompoundAssignmentOperators.exe
