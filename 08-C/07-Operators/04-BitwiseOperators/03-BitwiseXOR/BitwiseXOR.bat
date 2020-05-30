9b8bb324a6bb53f1915e2bcfdb2888aa187ba923
akshay3x <chandawalear@gmail.com>
2020-05-30 16:59:03 +0530

    Bitwise XOR operation

---
+++ 08-C/07-Operators/04-BitwiseOperators/03-BitwiseXOR/BitwiseXOR.bat
@@ -0,0 +1,9 @@
+
+del BitwiseXOR.obj
+del BitwiseXOR.exe
+
+cl.exe /c /EHsc BitwiseXOR.c
+
+link.exe BitwiseXOR.obj
+
+BitwiseXOR.exe
---
+++ 08-C/07-Operators/04-BitwiseOperators/03-BitwiseXOR/BitwiseXOR.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+int main(void)
+{
+	//function prototypes
+	int printBinaryFormOfNumber(unsigned int);
+
+	//variable declarations
+	unsigned int ac_a;
+	unsigned int ac_b;
+	unsigned int ac_result;
+
+	//code
+	//printf("\n");
+	printf("Enter 1st Integer(A) = ");
+	scanf("%u", &ac_a);
+	
+	printf("\n");
+	printf("Enter 2nd Integer(B) = ");
+	scanf("%u", &ac_b);
+	
+	printf("\n\n");
+	ac_result = ac_a ^ ac_b;
+	printf("Bitwise XORing Of \nA = (Decimal) %d  and B = (Decimal) %d gives result (Decimal) %d.\n\n", ac_a, ac_b, ac_result);
+
+	printBinaryFormOfNumber(ac_a);
+	printBinaryFormOfNumber(ac_b);
+	printBinaryFormOfNumber(ac_result);
+
+	return(0);
+}
+
+int printBinaryFormOfNumber(unsigned int decimal_number)
+{
+	//variable declarations
+	unsigned int quotient, remainder;
+	unsigned int num;
+	unsigned int binary_array[8];
+	int i;
+	
+	//code
+	for (i = 0; i < 8; i++)
+	binary_array[i] = 0;
+
+	printf("The binary form of the decimal integer %d is\t= ", decimal_number);
+	num = decimal_number;
+	i = 7;
+	
+	while (num != 0)
+	{
+	quotient = num / 2;
+	remainder = num % 2;
+	binary_array[i] = remainder;
+	num = quotient;
+	i--;
+	}
+	
+	for (i = 0; i < 8; i++)
+	printf("%u", binary_array[i]);
+	
+	printf("\n\n");
+	return(0);
+}
