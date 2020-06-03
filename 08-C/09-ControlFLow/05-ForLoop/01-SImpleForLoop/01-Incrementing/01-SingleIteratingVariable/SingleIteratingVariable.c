70e3dfcaf8233d7052d1bfeb6c510611cec5b0d8
akshay3x <chandawalear@gmail.com>
2020-06-04 01:01:47 +0530

    For Loop 01

---
+++ 08-C/09-ControlFLow/05-ForLoop/01-SImpleForLoop/01-Incrementing/01-SingleIteratingVariable/SingleIteratingVariable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//variable declarations
+	int ac_i;
+
+	//code
+	printf("\n");
+	printf("Printing digits 11 to 20:\n");
+	
+	for (ac_i = 11; ac_i <= 20; ac_i++)
+	{
+		printf("\t %d \n", ac_i);
+	}
+
+	printf("\n");
+
+	return(0);
+}
