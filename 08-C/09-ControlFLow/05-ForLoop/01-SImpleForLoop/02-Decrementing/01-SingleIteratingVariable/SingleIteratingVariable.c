ca70f47e2007ab0b9d9b635a4efa6c9e91505954
akshay3x <chandawalear@gmail.com>
2020-06-04 01:03:59 +0530

    For Loop 03

---
+++ 08-C/09-ControlFLow/05-ForLoop/01-SImpleForLoop/02-Decrementing/01-SingleIteratingVariable/SingleIteratingVariable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	//variable declarations
+	int ac_i;
+	
+	//code
+	printf("\n");
+	printf("Printing Digits 20 to 11 :\n");
+	
+	for (ac_i = 20; ac_i >= 11; ac_i--)
+	{
+		printf("\t %d \n", ac_i);
+	}
+	printf("\n");
+
+	return (0);
+}
