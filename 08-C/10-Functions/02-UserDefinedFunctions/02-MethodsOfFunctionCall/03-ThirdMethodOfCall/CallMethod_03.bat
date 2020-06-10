1d3653f64332ac7bf7a59fd39dcf4c75a1f131f6
akshay3x <chandawalear@gmail.com>
2020-06-10 17:00:30 +0530

    Function Calling 3

---
+++ 08-C/10-Functions/02-UserDefinedFunctions/02-MethodsOfFunctionCall/03-ThirdMethodOfCall/CallMethod_03.bat
@@ -0,0 +1,8 @@
+del CallMethod_03.obj
+del CallMethod_03.exe
+
+cl.exe /c /EHsc CallMethod_03.c
+
+link.exe CallMethod_03.obj
+
+CallMethod_03.exe
---
+++ 08-C/10-Functions/02-UserDefinedFunctions/02-MethodsOfFunctionCall/03-ThirdMethodOfCall/CallMethod_03.c
@@ -0,0 +1,106 @@
+#include <stdio.h> 
+
+int main(void)
+{
+	//function prototypes
+	void Ac_Function_Country();
+
+	//code
+	Ac_Function_Country();
+
+	return(0);
+}
+
+void Ac_Function_Country(void) //function definition
+{
+	// fucntion declarations
+	void Ac_Function_OfAMC(void);
+
+	// code
+	Ac_Function_OfAMC();
+	printf("\n");
+	printf("I live In India.");
+	printf("\n");
+
+}
+
+void Ac_Function_OfAMC(void) //function definition
+{
+	// function declarations
+	void Ac_Function_Surname(void);
+
+	// code
+	Ac_Function_Surname();
+	printf("\n");
+	printf("Of ASTROMEDICOMP.");
+
+}
+
+void Ac_Function_Surname(void) //function definition
+{
+	// function declarations
+	void Ac_Function_MiddleName(void);
+
+	// code
+	Ac_Function_MiddleName();
+	printf("\n");
+	printf("Chandawale");
+
+}
+
+void Ac_Function_MiddleName(void) //function definition
+{
+	// function declarations
+	void Ac_Function_FirstName(void);
+
+	// code
+	Ac_Function_FirstName();
+	printf("\n");
+	printf("Ramesh");
+
+}
+
+void Ac_Function_FirstName(void) //function definition
+{
+	// function declarations
+	void Ac_Function_Is(void);
+
+	// code
+	Ac_Function_Is();
+	printf("\n");
+	printf("Akshay");
+
+}
+
+void Ac_Function_Is(void) //function definition
+{
+	// function declarations
+	void Ac_Function_Name(void);
+
+	//code
+	Ac_Function_Name();
+	printf("\n");
+	printf("Is");
+
+}
+
+void Ac_Function_Name(void) //function definition
+{
+	// function declarations
+	void Ac_Function_My(void);
+
+	//code
+	Ac_Function_My();
+	printf("\n");
+	printf("Name");
+
+}
+
+// ****** User-Defined Functions' Definitions... ***
+void Ac_Function_My(void) //function definition
+{
+	//code
+	printf("\n");
+	printf("My");
+
+}
