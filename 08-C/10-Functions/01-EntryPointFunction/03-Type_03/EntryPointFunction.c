737990038765729022cafff6329b449377a762f5
akshay3x <chandawalear@gmail.com>
2020-06-10 16:39:14 +0530

    EntryPointFunction-03

---
+++ 08-C/10-Functions/01-EntryPointFunction/03-Type_03/EntryPointFunction.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int main(int argc)
+{
+	//code
+	printf("\n");
+	printf("Hello World !!!\n"); //Library function
+	printf("Number Of Command Line Arguments = %d\n\n", argc);
+
+	return(0);
+}
