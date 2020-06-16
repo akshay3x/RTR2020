0dcbab7789a648e1a2f4bec26b4ad367cdbbd410
akshay3x <chandawalear@gmail.com>
2020-06-16 12:32:20 +0530

    String Concatenation Using Library Function API

---
+++ 08-C/11-Arrays/01-OneDimensionalArray/06-StringOperations/05-StringConcatenation/01-UsingLibraryFunction_strcat/StringConcatenation.bat
@@ -0,0 +1,9 @@
+del StringConcatenation.obj
+
+del StringConcatenation.exe
+
+cl.exe /c /EHsc StringConcatenation.c
+
+link.exe StringConcatenation.obj
+
+StringConcatenation.exe
---
+++ 08-C/11-Arrays/01-OneDimensionalArray/06-StringOperations/05-StringConcatenation/01-UsingLibraryFunction_strcat/StringConcatenation.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define AC_MAX_STRING_LENGTH	512
+
+int main(void)
+{
+	//variable declaration
+	char ch_ac_str_one[AC_MAX_STRING_LENGTH];
+	char ch_ac_str_two[AC_MAX_STRING_LENGTH];
+
+	//code
+	printf("String Concatination:\n");
+	printf("Enter a String:");
+	gets_s(ch_ac_str_one, AC_MAX_STRING_LENGTH);
+
+	printf("Enter another String:");
+	gets_s(ch_ac_str_two, AC_MAX_STRING_LENGTH);
+
+	printf("\n\n");
+	printf("Before Concatination:\n");
+	printf("String one: %s\n", ch_ac_str_one);
+
+	printf("\n");
+	printf("String Two: %s\n", ch_ac_str_two);
+
+	//concatination Library Function
+	strcat(ch_ac_str_one, ch_ac_str_two);
+
+	printf("\n");
+	printf("After Concatination:\n");
+	printf("String one: %s\n", ch_ac_str_one);
+
+	printf("\n");
+	printf("String Two: %s\n", ch_ac_str_two);
+
+	return (0);
+}
