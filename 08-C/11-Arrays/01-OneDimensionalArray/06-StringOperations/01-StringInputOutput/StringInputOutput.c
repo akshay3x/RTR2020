2931da622c0e8b171cca00a2192fdb582bbec834
akshay3x <chandawalear@gmail.com>
2020-06-16 11:55:47 +0530

    String Input Output

--- 08-C/11-Arrays/01-OneDimensionalArray/06-StringOperations/01-StringInputOutput/StringInputOutput.c
+++ 08-C/11-Arrays/01-OneDimensionalArray/06-StringOperations/01-StringInputOutput/StringInputOutput.c
@@ -6,14 +6,14 @@ int main(void)
 {
 	//variable declaration
 	char ch_ac_array[AC_MAX_STRING_LENGTH];
-	
+
 	//code
 	printf("\n");
 	printf("Enter a String: ");
 	gets_s(ch_ac_array, AC_MAX_STRING_LENGTH);
-	
+
 	printf("\n");
 	printf("String entered: %s\n", ch_ac_array);
-	
+
 	return (0);
 }
