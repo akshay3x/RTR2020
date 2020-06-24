9c6b05e0d71981ff854641ffbed2b29109447be3
akshay3x <chandawalear@gmail.com>
2020-06-24 16:17:57 +0530

    Ponters Operations

---
+++ 08-C/14-Pointers/01-Variables/04-BracketsAndPointerOperations/BracketsAndPointerOperations.bat
@@ -0,0 +1,9 @@
+del BracketsAndPointerOperations.obj
+
+del BracketsAndPointerOperations.exe
+
+cl.exe /c /EHsc BracketsAndPointerOperations.c
+
+link.exe BracketsAndPointerOperations.obj
+
+BracketsAndPointerOperations.exe
---
+++ 08-C/14-Pointers/01-Variables/04-BracketsAndPointerOperations/BracketsAndPointerOperations.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//variable declarations
+	int num;
+	int *ptr = NULL;
+	int ans;
+	
+	//code
+	num = 5;
+	ptr = &num;
+
+	printf("\n");
+	printf(" num =		%d\n", num);
+	printf(" &num =		%p\n", &num);
+	printf(" *(&num) =	%d\n",*(&num));
+	printf(" ptr =		%p\n", ptr);
+	printf(" *ptr =		%d\n", *ptr);
+
+	printf("\n\n");
+	
+	printf(" (ptr + 10) =	%p\n", (ptr + 10));
+
+	printf(" *(ptr + 10) =	%d\n", *(ptr + 10));
+	
+	printf(" (*ptr + 10) =	%d\n", (*ptr + 10));
+
+	++*ptr;
+	printf(" ++*ptr =	%d\n", *ptr);
+	
+	*ptr++;
+	printf(" *ptr++ =	%d\n", *ptr);
+	
+	ptr = &num;
+	(*ptr)++;
+	printf(" (*ptr)++ =	%d\n", *ptr);
+	
+	return (0);
+}
