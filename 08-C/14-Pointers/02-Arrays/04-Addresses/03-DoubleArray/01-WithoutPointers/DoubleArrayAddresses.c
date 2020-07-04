066e8414d93abf726520bdcd6009b37421ed0332
akshay3x <chandawalear@gmail.com>
2020-07-04 13:54:18 +0530

    modification

--- 08-C/14-Pointers/02-Arrays/04-Addresses/03-DoubleArray/01-WithoutPointers/DoubleArrayAddresses.c
+++ 08-C/14-Pointers/02-Arrays/04-Addresses/03-DoubleArray/01-WithoutPointers/DoubleArrayAddresses.c
@@ -15,13 +15,13 @@ int main(void)
 	printf("\n\n");
 	printf("--- Array Elements ---\n");
 	for(ac_i = 0; ac_i < 10; ac_i++)
-		printf(" double_array[%d] = %f\n", ac_i, double_array[ac_i]);
+		printf(" double_array[%d] = %lf\n", ac_i, double_array[ac_i]);
 
 	//displaying array elements with addresses
 	printf("\n\n");
 	printf("--- Array Elements With Addresses---\n");
 	for(ac_i = 0; ac_i < 10; ac_i++)
-		printf(" double_array[%d] = %f\t Address = %p\n", ac_i, double_array[ac_i], &double_array[ac_i]);
+		printf(" double_array[%d] = %lf\t Address = %p\n", ac_i, double_array[ac_i], &double_array[ac_i]);
 
 	return (0);
 }
