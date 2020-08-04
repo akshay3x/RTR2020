2b3546edf907a831da5b2442cbff32a8a8d863c0
akshay3x <chandawalear@gmail.com>
2020-08-04 20:27:56 +0530

    Blue Window With Double Buffering

---
+++ 01-Windows/01-FixedFunctionalPipeline/03-DoubleBuffer/main.cpp
@@ -0,0 +1,388 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <GL/GL.h>
+
+#include "resource.h"
+
+//library inclusion/linking
+#pragma comment(lib, "opengl32.lib")
+
+//macro definitions
+#define WIN_WIDTH 800
+#define WIN_HIGHT 600
+
+//window procedure
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//global variable declarations
+HWND ghwnd = NULL;
+HDC ghdc = NULL;
+HGLRC ghrc = NULL;
+
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev = {sizeof(WINDOWPLACEMENT)};
+
+bool gbFullscreen = false;
+bool gbActiveWindow = false;
+FILE *gpFile = NULL;
+
+//WinMain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	
+	//function declations
+	void initialize(void);
+	void display(void);
+	void uninitialize(void);
+	
+	//variable declarations
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("OpenGL");
+	RECT rect;
+	bool bDone = false;
+	
+	UINT VerPos;
+	UINT HorPos;
+
+	//code
+	//Creating log file
+	if(fopen_s(&gpFile, "Log.txt", "w") != 0)
+	{
+		MessageBox(NULL, TEXT("Can Not Open Desired File..."),TEXT("Error"), MB_OK);
+		exit(0);
+	}
+	else
+	{
+		fprintf(gpFile, "DEBUG:Log File Created Successfully\n");
+	}
+	
+	//initializing winndow class
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.cbWndExtra = 0;
+	wndclass.cbClsExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hIcon = LoadIcon(hInstance , MAKEINTRESOURCE(MYICON));
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hIconSm = LoadIcon(hInstance , MAKEINTRESOURCE(MYICON));
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+
+
+	//Registering above class
+	RegisterClassEx(&wndclass);
+	fprintf(gpFile, "DEBUG:Class Registered Successfully\n");
+	
+	//SystemParametersInfo
+	SystemParametersInfo(SPI_GETWORKAREA, 0, &rect, 0);
+
+	HorPos = (rect.right - WIN_WIDTH) / 2;
+	VerPos = (rect.bottom - WIN_HIGHT) / 2;
+
+	//creating window
+	hwnd = CreateWindowEx(	WS_EX_APPWINDOW,
+							szAppName,
+							TEXT("OpenGL Window"),
+							WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+							HorPos,
+							VerPos,
+							WIN_WIDTH,
+							WIN_HIGHT,
+							NULL,
+							NULL,
+							hInstance,
+							NULL);
+
+	fprintf(gpFile, "DEBUG:Window Created\n");
+
+	ghwnd = hwnd;
+	initialize();
+	ShowWindow(hwnd, iCmdShow);
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+
+	//MessageLoop
+	while(bDone == false)
+	{
+		if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if(msg.message == WM_QUIT)
+				bDone = true;
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			if(gbActiveWindow == true)
+			{
+				//HERE YOU SHOULD CALL UPDATE FUNCTION FOR OPENGL RENDERING
+				//HERE YOU SHOULD CALL DISPLAY FUNCTION FOR OPENGL RENDERING
+				display();
+			}
+		}
+	}
+
+	return((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//function declarations
+	void ToggleFullscreen(void);
+	void uninitialize(void);
+	void resize(int, int);
+	void display(void);
+
+	//code
+	switch(iMsg)
+	{
+		case WM_CREATE:
+			fprintf(gpFile, "DEBUG:WM_CREATE Recieved\n");
+			break;
+
+		case WM_SETFOCUS:
+			gbActiveWindow = true;
+			break;
+
+		case WM_KILLFOCUS:
+			gbActiveWindow = false;
+			break;
+
+		case WM_ERASEBKGND:
+			return(0);
+
+		case WM_SIZE:
+			resize(LOWORD(lParam), HIWORD(lParam));
+			break;
+
+		case WM_KEYDOWN:
+			switch(wParam)
+			{
+				case 0x46:
+				case 0x66:
+					ToggleFullscreen();
+					break;
+
+				case VK_ESCAPE:
+					DestroyWindow(hwnd);
+					break;
+
+				default:
+					break;
+			}
+			break;
+
+		case WM_CLOSE:
+			DestroyWindow(hwnd);
+			break;
+
+		case WM_DESTROY:
+			fprintf(gpFile, "DEBUG:WM_DESTROY Recieved\n");
+			uninitialize();
+			PostQuitMessage(0);
+			break;
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+void ToggleFullscreen(void)
+{
+	//variable declarations
+	MONITORINFO mInfo = {sizeof(MONITORINFO)};
+
+	//code
+	if(gbFullscreen == false)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+		if(dwStyle && WS_OVERLAPPEDWINDOW)
+		{
+			if(GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mInfo))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, (dwStyle & ~(WS_OVERLAPPEDWINDOW)));
+				SetWindowPos(	ghwnd,
+								HWND_TOP,
+								mInfo.rcMonitor.left,
+								mInfo.rcMonitor.top,
+								mInfo.rcMonitor.right - mInfo.rcMonitor.left,
+								mInfo.rcMonitor.bottom - mInfo.rcMonitor.top,
+								SWP_NOZORDER | SWP_FRAMECHANGED);
+			}
+		}
+		ShowCursor(FALSE);
+		gbFullscreen = true;
+	}
+
+	else
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, (dwStyle | WS_OVERLAPPEDWINDOW));
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd,
+					HWND_TOP,
+					0,
+					0,
+					0,
+					0,
+					SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+
+				ShowCursor(TRUE);
+				gbFullscreen = false;
+	}
+}
+
+void initialize(void)
+{
+	//function declarations
+	void resize(int, int);
+	
+	//variable declarations
+	PIXELFORMATDESCRIPTOR pfd = {sizeof(PIXELFORMATDESCRIPTOR)};
+	int iPixelFormatIndex;
+
+	//code
+	ghdc = GetDC(ghwnd);
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+	//memset((void *) &pfd, 0, sizeof(PIXELFORMATDESCRIPTOR))
+
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	
+	pfd.nVersion = 1;
+	//opengl version
+	
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	//flags for minimum values to be modified
+	//can alo be BITMAP
+	//pixel of highest quality OpenGL
+	
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	//type of pixel required for rendering
+
+	pfd.cColorBits = 32;
+	//maximum color bits per pixel
+	
+	pfd.cRedBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cAlphaBits = 8;
+	
+	//pfd.cDepthBits = 8;
+	
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+	//choose pixel format from ghdc of type pfd and return its index
+	//if index is 0, it is not found or error
+	if(iPixelFormatIndex == 0)
+	{
+		fprintf(gpFile, "DEBUG:ChoosePixelFormat() Failed\n");
+		DestroyWindow(ghwnd);
+	}
+
+	if(SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE)
+		//set the pixel format from ghdc to pfd 
+	{
+		fprintf(gpFile, "DEBUG:SetPixelFormat() Failed\n");
+		DestroyWindow(ghwnd);
+	}
+	
+	ghrc = wglCreateContext(ghdc);
+	//bridging APIs
+	//WSI windowing Sysyem Integration
+	
+	//Windows: Window Graphics Library
+	//MAC: CGL Core Graphics Library(old AGL)
+	//Linux: GLX Graphics Library for Xwindows
+	//Android: EGL Embedded Graphics Library
+	//iOS: EAGL
+	//WebGL: EGL
+	
+	//GLRC: handle to graphics library redering context
+	
+	if(ghrc == NULL)
+	{
+		fprintf(gpFile, "DEBUG:wglCreateContext() Failed\n");
+		DestroyWindow(ghwnd);//added new
+	}
+
+
+	//make the corrent contest as ghrc instead of ghdc
+	if(wglMakeCurrent(ghdc, ghrc) == FALSE)
+	{
+		fprintf(gpFile, "DEBUG:wgl_MakeContext() Failed\n");
+		DestroyWindow(ghwnd);
+	}
+
+	//Set ClearColor
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+
+	//WarmUp resize() Call
+	resize(WIN_WIDTH, WIN_HIGHT);
+}
+
+void resize(int width, int hight)
+{
+	//code
+	if(hight == 0)
+		hight = 1;
+
+	//
+	glViewport(0, 0, GLsizei(width), GLsizei(hight));
+}
+
+void display(void)
+{
+	//code
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	SwapBuffers(ghdc);
+}
+
+void uninitialize(void)
+{
+
+	if(gbFullscreen == true)
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, (dwStyle | WS_OVERLAPPEDWINDOW));
+		SetWindowPos(ghwnd,
+					HWND_TOP,
+					0,
+					0,
+					0,
+					0,
+					SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+		ShowCursor(TRUE);
+	}
+
+	//deselecting rendering context
+	if(wglGetCurrentContext() == ghrc)
+	{
+		wglMakeCurrent(NULL, NULL);
+	}
+
+	//releasing rendering context handle
+	if(ghrc)
+	{
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+	}
+	
+	//releasing device context handle
+	if(ghdc)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	//closing log file
+	if(gpFile)
+	{
+		fprintf(gpFile, "DEBUG:Log File Closed\n");
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+}
