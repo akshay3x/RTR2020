bae4988ba013ac6f9c6ff26012aeb24d420ac8b2
akshay3x <chandawalear@gmail.com>
2020-10-27 01:11:32 +0530

    Pyramid and Cube Rotations

---
+++ 01-Windows/01-FixedFunctionalPipeline/39-PyramidAndCube/build.bat
@@ -0,0 +1,13 @@
+del main.obj 
+
+del Application.exe
+
+del resource.res
+
+cl.exe /c /EHsc main.cpp
+
+rc resource.rc 
+
+link.exe main.obj resource.res user32.lib gdi32.lib /SUBSYSTEM:WINDOWS /out:Application.exe
+
+Application.exe
