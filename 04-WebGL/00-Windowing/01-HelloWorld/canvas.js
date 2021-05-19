71bfca59488c9d09b0ce5e86bb74c5ee82c4281a
akshay3x <chandawalear@gmail.com>
2021-05-19 21:56:00 +0530

    Hello World!!!-[WebGL]

---
+++ 04-WebGL/00-Windowing/01-HelloWorld/canvas.js
@@ -0,0 +1,50 @@
+function main()
+{
+	//step-1 get canvas from dom
+	var canvas = document.getElementById("MyID");
+
+	if(!canvas)
+	{
+		console.log("Obtaining Canvas Failed!");
+	}
+	else
+	{
+		console.log("Obtaining Canvas Succeded");
+	}
+
+	//retrive width and hight of canvas
+	console.log("Canvas Width = " + canvas.width);
+	console.log("\n Canvas hight = " + canvas.height);
+
+	//get drawing context from canvas
+	var context = canvas.getContext("2d");
+
+	if(!context)
+	{
+		console.log("Obtaining context Failed!");
+	}
+	else
+	{
+		console.log("Obtaining context Succeded");
+	}
+
+	//paint backgroud by black color
+	//context.fillStyle = "black";
+	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	//centre the text
+	context.textAllign = "center"; //horrizontal center
+	context.textBaseline = "middle"; //verticle center
+
+	//font
+	context.font = "30px sans-serif";
+
+	//string to be display
+	var str = "Hello World!!!";
+
+	//text color
+	context.fillStyle = "white";
+
+	//display the text
+	context.fillText(str, canvas.width/2, canvas.height/2);
+}
